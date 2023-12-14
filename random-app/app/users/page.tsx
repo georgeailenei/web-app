12401450c21bf345db622b38b17d6a8046299191
georgeailenei <george.ailenei@gmail.com>
2023-12-14 12:02:40 +0000

    layout and navigation/Link

--- random-app/app/users/page.tsx
+++ random-app/app/users/page.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 const UsersPage = () => {
-  return <div> This is the users page </div>;
+  return (
+    <>
+      <div> This is the users page </div>
+      <Link href="/users/chat-room">Chat Room</Link>
+    </>
+  );
 };
 
 export default UsersPage;
